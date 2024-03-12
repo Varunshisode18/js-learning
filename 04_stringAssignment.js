7b1b1d114b8640482de5876198a7d4ba1c572ff5
Varunshisode18 <115209424+Varunshisode18@users.noreply.github.com>
2024-03-12 11:39:05 +0530

    String Assignment On hobby and Dream Company

---
+++ 04_stringAssignment.js
@@ -0,0 +1,19 @@
+
+function stringBasics(){
+
+    var dreamCompany = "Amdocs";
+    console.log(`My Dream Company is "${dreamCompany}".`);
+
+   var hobby1 = "Playing Cricket" ;
+   var hobby2 = "Running";
+   var hobby3 = "Ridding";
+   console.log(`My First Hobby is ${hobby1} and Length is ${hobby1.length}.`);
+   console.log(`My First Hobby is ${hobby2} and Length is ${hobby2.length}.`);
+   console.log(`My First Hobby is ${hobby3} and Length is ${hobby3.length}.`);
+
+   var add = hobby1.length + hobby2.length + hobby3.length;
+   console.log(`Addition Of  Hobbies Letters is = ${add}.`);
+
+}
+
+stringBasics();
