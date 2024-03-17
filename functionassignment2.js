ec7a6acf151745fe1291ce480d69b072b7ce656f
Varunshisode18 <115209424+Varunshisode18@users.noreply.github.com>
2024-03-17 16:57:39 +0530

    Function assignment

--- functionassignment2.js
+++
@@ -1,8 +0,0 @@
-
-function bankDetails(bankName , accountNo , location ,pinCode){
-    console.log("Bank Details :" , bankName , accountNo , location , pinCode);
-
-}
-bankDetails("City Bank ", 5097070224 ,' pune ' , 431202);
-bankDetails("Axis Bank ", 1234070224 ,' Mumbai ' , 121202);
-bankDetails("Central Bank ", 56677070 ,' pune ' , 432202);
