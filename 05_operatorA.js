0cae6eb888faa4c6b5c1b83111f524439a5f0850
Varunshisode18 <115209424+Varunshisode18@users.noreply.github.com>
2024-03-17 16:53:36 +0530

    Assignment Of the Arithmatic Operator

---
+++ 05_operatorA.js
@@ -0,0 +1,61 @@
+console.log("_______________________________ Step1_________________________________");
+
+
+function squareOfWordLength(first){
+    
+    var lengthofjs = first.length;
+    console.log(`Length of the Javascript is ${lengthofjs}`);
+
+    var squareofjs = lengthofjs ** 2;
+    console.log(`Square of javascript is ${squareofjs}`);
+
+
+}
+
+squareOfWordLength("JavaScript");
+
+function squareOfWordLength(second){
+
+
+    console.log("-------------------------------");
+    var lengthofjs = second.length;
+    console.log(`Length of the Javascript is ${lengthofjs}`);
+
+    var squareofjs = lengthofjs ** 2;
+    console.log(`Square of javascript is ${squareofjs}`)
+
+
+}
+squareOfWordLength("Google Chrome");
+
+
+function squareOfWordLength(third){
+
+
+    
+    var lengthofjs = third.length;
+    console.log(`Length of the Javascript is ${lengthofjs}`);
+
+    var squareofjs = lengthofjs ** 2;
+    console.log(`Square of javascript is ${squareofjs}`);
+
+    
+
+
+}
+squareOfWordLength("Developer Smart");
+
+
+console.log("_______________________________ Step1_________________________________");
+
+
+function demo (){
+    var line = "I Am Angular Developer";
+    var lengthofline = line.length;
+    console.log(`Length of the string is : ${lengthofline}`);
+    
+    console.log(`length of input divided by no of word : ${lengthofline / 4}`);
+    console.log(`length of input divided by no of word : ${lengthofline * 4}`);
+
+}
+demo();
